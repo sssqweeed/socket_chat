0daaf30c134be4a395a54c9fabff02f47f954095
Mikhail <mischav44@gmail.com>
2020-12-14 12:37:20 +0300

    Initial Commit

---
+++ socket_chat/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  socket_chat
+//
+//  Created by Михаил Воронин on 14.12.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
